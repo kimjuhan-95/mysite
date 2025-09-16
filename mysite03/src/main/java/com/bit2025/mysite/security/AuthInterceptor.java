f4459cd3631832e8c92b1bb49373193c9c4001c6
김주한 <jhkim9596@gmail.com>
2025-09-16 17:47:49 +0900

    asd

--- mysite03/src/main/java/com/bit2025/mysite/security/AuthInterceptor.java
+++ mysite03/src/main/java/com/bit2025/mysite/security/AuthInterceptor.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-public class Authlnterceptor implements HandlerInterceptor {
+public class AuthInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
